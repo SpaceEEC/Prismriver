006f35a51910a5bcc68a1e482e12679a924f9c45
SpaceEEC <spaceeec@yahoo.com>
2018-09-18 10:33:20 +0200

    Add project files.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FFmpeg;
+
+namespace Sandbox
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			String fileName = @"test.mp4";
+			String fileOut = @"output.raw";
+			using (FFmpeg.FFmpeg ffmpeg = new FFmpeg.FFmpeg(fileName, fileOut))
+				ffmpeg.DoStuff();
+
+			Console.Read();
+		}
+	}
+}
