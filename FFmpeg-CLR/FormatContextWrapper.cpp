cf5619bd367daaf7ea4a0d7c557606e5a9f03eaa
SpaceEEC <spaceeec@yahoo.com>
2018-09-25 23:10:43 +0200

    fix(FormatContextWrapper): open AVIOContext if using a file

--- FFmpeg-CLR/FormatContextWrapper.cpp
+++ FFmpeg-CLR/FormatContextWrapper.cpp
@@ -16,6 +16,8 @@ namespace FFmpeg
 			}
 			else
 			{
+				if (this->file_ != nullptr) avio_closep(&pContext->pb);
+
 				avformat_free_context(pContext);
 				this->formatContext = nullptr;
 			}
@@ -26,6 +28,7 @@ namespace FFmpeg
 			Marshal::FreeHGlobal(static_cast<IntPtr>(const_cast<char*>(this->file_)));
 			this->file_ = nullptr;
 		}
+
 		if (this->ioContextWrapper_ != nullptr)
 		{
 			delete this->ioContextWrapper_;
@@ -54,20 +57,37 @@ namespace FFmpeg
 	{
 		if (this->opened_) return E_NOT_VALID_STATE;
 
-		AVOutputFormat* pOutputFormat = av_guess_format("mp3", NULL, NULL);
-		if (pOutputFormat == nullptr) return E_UNEXPECTED;
+		AVOutputFormat* pOutputFormat = av_guess_format(this->file_ == nullptr ? "flac" : NULL, this->file_, NULL);
+		if (pOutputFormat == nullptr) return E_FAIL;
 
 		HRESULT hr = S_OK;
-		if (this->ioContextWrapper_ != nullptr && FAILED(hr = this->ioContextWrapper_->openWrite()))
-			return hr;
-			
+	
 		AVFormatContext* pFormatContext = nullptr;
 		if (FAILED(hr = avformat_alloc_output_context2(&pFormatContext, pOutputFormat, NULL, this->file_)))
 			return hr;
-		this->formatContext = pFormatContext;
 
-		if (this->ioContextWrapper_ != nullptr) this->formatContext->pb = this->ioContextWrapper_->ioContext;
-		
+		if (this->ioContextWrapper_ != nullptr)
+		{
+			if (FAILED(hr = this->ioContextWrapper_->openWrite()))
+			{
+				avformat_free_context(pFormatContext);
+
+				return hr;
+			}
+
+			pFormatContext->pb = this->ioContextWrapper_->ioContext;
+		}
+		else if ((pOutputFormat->flags & AVFMT_NOFILE) == 0)
+		{
+			if (FAILED(hr = avio_open(&pFormatContext->pb, this->file_, AVIO_FLAG_WRITE)))
+			{
+				avformat_free_context(pFormatContext);
+
+				return hr;
+			}
+		}
+
+		this->formatContext = pFormatContext;
 		return hr;
 	}
 }
