07a1f9a2eca80e837ade4a5703530321bf4e77aa
SpaceEEC <spaceeec@yahoo.com>
2018-09-26 20:56:23 +0200

    refactor(AVException): move into FFmpeg namespace

--- FFmpeg-CLR/AVException.h
+++ FFmpeg-CLR/AVException.h
@@ -9,23 +9,26 @@ extern "C"
 
 using namespace System;
 
-public ref class AVException : Exception
+namespace FFmpeg
 {
-public:
-	AVException(String^ message) : Exception(message, nullptr) {}
-	AVException(String^ message, Exception^ e) : Exception(message, e) {}
-
-	AVException(HRESULT hr) : AVException(hr, nullptr) {}
-	AVException(HRESULT hr, Exception^ e) :
-		Exception(AVException::GetStringFromAVerror(hr), e)
+	public ref class AVException : Exception
 	{
-		this->HResult = hr;
-	}
+	public:
+		AVException(String^ message) : Exception(message, nullptr) {}
+		AVException(String^ message, Exception^ e) : Exception(message, e) {}
 
-	static String^ GetStringFromAVerror(HRESULT hr)
-	{
-		char errbuf[AV_ERROR_MAX_STRING_SIZE];
-		av_make_error_string(reinterpret_cast<char*>(&errbuf), AV_ERROR_MAX_STRING_SIZE, hr);
-		return gcnew String(errbuf);
-	}
-};
+		AVException(HRESULT hr) : AVException(hr, nullptr) {}
+		AVException(HRESULT hr, Exception^ e) :
+			Exception(AVException::GetStringFromAVerror(hr), e)
+		{
+			this->HResult = hr;
+		}
+
+		static String^ GetStringFromAVerror(HRESULT hr)
+		{
+			char errbuf[AV_ERROR_MAX_STRING_SIZE];
+			av_make_error_string(reinterpret_cast<char*>(&errbuf), AV_ERROR_MAX_STRING_SIZE, hr);
+			return gcnew String(errbuf);
+		}
+	};
+}
