32507b8c0b78069d1d48741467eedd4618033f01
SpaceEEC <spaceeec@yahoo.com>
2018-09-27 18:45:05 +0200

    perf(IO): keep managed array chunk sizes below large object threshhold

--- FFmpeg-CLR/IO.cpp
+++ FFmpeg-CLR/IO.cpp
@@ -16,45 +16,26 @@ namespace FFmpeg
 			Stream^ ms = static_cast<Stream^>(static_cast<GCHandle>(static_cast<IntPtr>(opaque)).Target);
 			if (!ms->CanRead) return -1;
 
-			array<unsigned char>^ hBuffer = gcnew array<unsigned char>(bufSize);
-
-			int read = ms->Read(hBuffer, 0, bufSize);
-
-			{
-				pin_ptr<unsigned char> pinned = &hBuffer[0];
-				memcpy_s(
-					buf,
-					sizeof(unsigned char) * read,
-					static_cast<void*>(pinned),
-					sizeof(unsigned char) * read
-				);
-			}
-
-			delete hBuffer;
-
-			return read == 0 ? AVERROR_EOF : read;
-		}
-
-		int WriteFunc(void* opaque, unsigned char* buf, int bufSize)
-		{
-			Stream^ ms = static_cast<Stream^>(static_cast<GCHandle>(static_cast<IntPtr>(opaque)).Target);
-			if (!ms->CanWrite) return -1;
-
-			array<unsigned char>^ hBuffer = gcnew array<unsigned char>(bufSize);
-
-			{
-				pin_ptr<unsigned char> pinned = &hBuffer[0];
-				memcpy_s(
-					static_cast<void*>(pinned),
-					bufSize,
-					buf,
-					bufSize
-				);
-			}
-
+			int chunkSize = FFMIN(bufSize, 81920);
+			array<unsigned char>^ hBuffer = gcnew array<unsigned char>(chunkSize);
+			int readTotal = 0;
+			int read;
 			try
 			{
-				ms->Write(hBuffer, 0, bufSize);
+				while ((read = ms->Read(hBuffer, 0, FFMIN(chunkSize, bufSize))) != 0)
+				{
+					pin_ptr<unsigned char> pBuffer = &hBuffer[0];
+					memcpy_s(
+						buf,
+						read,
+						pBuffer,
+						read
+					);
+
+					readTotal += read;
+					buf += read;
+					bufSize -= read;
+				}
 			}
 			catch (Exception^)
 			{
@@ -64,6 +45,44 @@ namespace FFmpeg
 			{
 				delete hBuffer;
 			}
+
+			return readTotal == 0 ? AVERROR_EOF : readTotal;
+		}
+
+		int WriteFunc(void* opaque, unsigned char* buf, int bufSize)
+		{
+			Stream^ ms = static_cast<Stream^>(static_cast<GCHandle>(static_cast<IntPtr>(opaque)).Target);
+			if (!ms->CanWrite) return -1;
+
+			int chunkSize = FFMIN(bufSize, 81920);
+			array<unsigned char>^ hBuffer = gcnew array<unsigned char>(chunkSize);
+			do
+			{
+				int read = FFMIN(chunkSize, bufSize);
+
+				pin_ptr<unsigned char> pBuffer = &hBuffer[0];
+				memcpy_s(
+					pBuffer,
+					read,
+					buf,
+					read
+				);
+
+				try
+				{
+					ms->Write(hBuffer, 0, read);
+				}
+				catch (Exception^)
+				{
+					delete hBuffer;
+					return -1;
+				}
+
+				buf += read;
+				bufSize -= read;
+			} while (bufSize != 0);
+
+			delete hBuffer;
 
 			return 0;
 		}
