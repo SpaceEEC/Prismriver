f9651d0d56736aee329027bada2027d6c7c4f0d3
SpaceEEC <spaceeec@yahoo.com>
2018-09-28 21:23:21 +0200

    fix(IO): match parameter names of declaration and implementation

--- FFmpeg-CLR/IO.h
+++ FFmpeg-CLR/IO.h
@@ -4,8 +4,8 @@ namespace FFmpeg
 {
 	namespace IO
 	{
-		int ReadFunc(void* opaque, unsigned char* buf, int buf_size);
-		int WriteFunc(void* opaque, unsigned char* buf, int buf_size);
+		int ReadFunc(void* opaque, unsigned char* buf, int bufSize);
+		int WriteFunc(void* opaque, unsigned char* buf, int bufSize);
 		long long SeekFunc(void* opaque, long long offset, int whence);
 	}
 }
